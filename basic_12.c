056b5dee6fc62eda3e47fda168f9a39764fb470b
ruiwen0307 <67383973+ruiwen0307@users.noreply.github.com>
2022-01-16 00:30:04 +0800

    Create basic_12.c

---
+++ basic_12.c
@@ -0,0 +1,18 @@
+//https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30761
+#include <stdio.h>
+
+int f(int n){
+    if(n == 0 || n == 1){
+        return n + 1;
+    }
+    else{
+        return f(n-1)+f(n/2);
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", f(n));
+}
+
