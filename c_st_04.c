d4f0a8d8caeb27e75ff19a29fb0643e6ae9c4603
ruiwen0307 <67383973+ruiwen0307@users.noreply.github.com>
2022-01-16 17:02:29 +0800

    Create c_st_04.c

---
+++ c_st_04.c
@@ -0,0 +1,10 @@
+//https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=518
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char n;
+    while(scanf("%c", &n) != EOF){
+        printf("%d\n", n);
+    }
+}
